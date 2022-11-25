225461ae9fcaccf22767e77dc8705aa4edadbf0e
Palpatin-lll <117033246+Palpatin-lll@users.noreply.github.com>
2022-11-25 20:36:35 +0300

    ReadAndSave
    A program that reads and saves a file through the registry in WPF C#

---
+++ 11.10.2022/11.10.2022/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Data;
+using ExcelDataReader;
+
+namespace _11._10._2022
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IExcelDataReader edr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.a.SelectAllCells();
+            this.a.ClipboardCopyMode = DataGridClipboardCopyMode.ExcludeHeader;
+            ApplicationCommands.Copy.Execute(null, this.a);
+            this.a.UnselectAllCells();
+            String result = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "(.csv)|.csv| (.xml)|.xml";
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile(), System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(result);
+                    sw.Close();
+                }
+            }
+        }
+
+        private DataView readFile(string fileNames)
+        {
+            var extension = fileNames.Substring(fileNames.LastIndexOf('.'));
+            FileStream stream = File.Open(fileNames, FileMode.Open, FileAccess.Read);
+            if (extension == ".csv")
+                edr = ExcelReaderFactory.CreateCsvReader(stream);
+            else if (extension == ".xml")
+                edr = ExcelReaderFactory.CreateCsvReader(stream);
+            var conf = new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                {
+                    UseHeaderRow = true
+                }
+            };
+            DataSet dataSet = edr.AsDataSet(conf);
+            DataView dtView = dataSet.Tables[0].AsDataView();
+            edr.Close();
+            return dtView;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                a.ItemsSource = readFile(openFileDialog.FileName);
+        }
+    }
+}
